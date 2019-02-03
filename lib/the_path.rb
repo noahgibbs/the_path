806fedefb5e13dabc90cbe2e856036ed61e12c47
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-02-02 17:42:55 -0800

    Add open-details and click-details reports

--- lib/the_path.rb
+++ lib/the_path.rb
@@ -8,6 +8,8 @@ module ThePath
         LIST_FIELDS = [:id, :web_id, :name, :contact, :permission_reminder, :use_archive_bar, :campaign_defaults, :notify_on_subscribe, :notify_on_unsubscribe, :date_created, :list_rating, :email_type_option, :subscribe_url_short, :subscribe_url_long, :beamer_address, :visibility, :double_optin, :has_welcome, :marketing_permissions, :modules, :stats]
         MEMBER_FIELDS = [:id, :email_address, :unique_email_id, :email_type, :status, :merge_fields, :interests, :stats, :ip_signup, :timestamp_signup, :ip_opt, :timestamp_opt, :member_rating, :last_changed, :language, :vip, :email_client, :location, :tags_count, :tags]
         CAMPAIGN_FIELDS = [:id, :web_id, :type, :create_time, :archive_url, :long_archive_url, :status, :emails_sent, :send_time, :content_type, :needs_block_refresh, :has_logo_merge_tag, :resendable, :recipients, :settings, :tracking, :report_summary, :delivery_status]
+        OPEN_DETAILS_FIELDS = [:campaign_id, :list_id, :list_is_active, :email_id, :email_address, :vip, :opens_count, :opens]
+        CLICK_DETAILS_FIELDS = [:id, :url, :total_clicks, :click_percentage, :unique_clicks, :unique_click_percentage, :last_click, :campaign_id]
 
         def initialize(repo_path:, freshness: 15 * 60)
             @freshness = freshness
@@ -29,7 +31,7 @@ module ThePath
                     FileUtils.mkdir_p list_dir_path
                     @cached_resources["list_#{list_id}/members"] = proc do
                         members_data = fully_load(proc { gibbon_client.lists(list_id).members }, :members)
-                        members_data.map { |item| item.slice(*MEMBER_FIELDS) }
+                        members_data = members_data.map { |item| item.slice(*MEMBER_FIELDS) }
                         possibly_typos = MEMBER_FIELDS - members_data[0].keys
                         unless possibly_typos.empty?
                             STDERR.puts "May have a typo in field: #{possibly_typos.inspect} for members"
@@ -48,6 +50,35 @@ module ThePath
                 possibly_typos = CAMPAIGN_FIELDS - campaigns[0].keys
                 unless possibly_typos.empty?
                     STDERR.puts "May have a typo in field: #{possibly_typos.inspect} for campaigns"
+                end
+
+                campaigns.each do |campaign|
+                    campaign_id = campaign[:id]
+                    campaign_dir_path = File.join(@repo_path, "campaign_#{campaign_id}")
+                    FileUtils.mkdir_p campaign_dir_path
+                    @cached_resources["campaign_#{campaign_id}/open_details"] = proc do
+                        open_details_data = fully_load(proc { gibbon_client.reports(campaign_id).open_details }, :members)
+                        open_details_data = open_details_data.map { |item| item.slice(*OPEN_DETAILS_FIELDS) }
+                        if open_details_data.size > 0
+                            possibly_typos = OPEN_DETAILS_FIELDS - open_details_data[0].keys
+                            unless possibly_typos.empty?
+                                STDERR.puts "May have a typo in field: #{possibly_typos.inspect} for members"
+                            end
+                        end
+                        open_details_data
+                    end
+
+                    @cached_resources["campaign_#{campaign_id}/click_details"] = proc do
+                        click_details_data = fully_load(proc { gibbon_client.reports(campaign_id).click_details }, :urls_clicked)
+                        click_details_data = click_details_data.map { |item| item.slice(*CLICK_DETAILS_FIELDS) }
+                        possibly_typos = CLICK_DETAILS_FIELDS - click_details_data[0].keys
+                        unless possibly_typos.empty?
+                            STDERR.puts "May have a typo in field: #{possibly_typos.inspect} for members"
+                        end
+                        click_details_data
+                    end
+
+
                 end
 
                 campaigns
@@ -83,7 +114,17 @@ module ThePath
             # There are several clever ways to use cur_batch.body[:total_items] here. Instead, we just keep
             # querying until we get less than the batch size and call it a day.
             loop do
-                cur_batch = gibbon_request.call().retrieve(params: { "count": 50, "offset": offset.to_s })
+                cur_batch = nil
+                attempts = 0
+                until cur_batch
+                    attempts += 1
+                    begin
+                        cur_batch = gibbon_request.call().retrieve(params: { "count": 50, "offset": offset.to_s })
+                    rescue Gibbon::MailChimpError
+                        raise "Couldn't load data (field #{data_field.inspect}) due to MailChimpError! #{$!.inspect}" if attempts > 5
+                        STDERR.puts "Retrying after MailChimpError, attempt #{attempts.inspect}: #{$!.inspect}"
+                    end
+                end
                 STDERR.puts "Concatenating current batch, existing size: #{data.size}, total size: #{cur_batch.body[:total_items].inspect}, data field: #{data_field.inspect}, new obj type: #{cur_batch.body[data_field].class}"
                 if cur_batch.body[data_field] == nil
                     STDERR.puts "Error in return value:"
@@ -101,18 +142,18 @@ module ThePath
         # Return a list of all currently-known resource names.
         # Note that loading a resource can make other resources knowable,
         # which is a serious flaw in the current system.
-        def known_resources
-            @cached_resources.keys
+        def known_resources(match: "")
+            @cached_resources.keys.select { |key| key[match] }
         end
 
         # This is another bad idea for normal use - it fully loads every piece of
         # MailChimp data it can find, likely taking many HTTP requests to do so.
         # It will still use the cache files if freshness allows.
-        def load_all_resources(freshness: @freshness)
+        def load_all_resources(freshness: @freshness, match: "")
             loaded_resources = []
             loop do
                 known_resources = @cached_resources.keys
-                new_resources = known_resources - loaded_resources
+                new_resources = known_resources(match: match) - loaded_resources
                 return if new_resources.empty?
                 new_resources.each do |new_resource|
                     STDERR.puts "Loading new resource: #{new_resource.inspect}"
